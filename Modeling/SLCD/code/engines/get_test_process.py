7caf4563ade3ef9ea0f84804165b73396d128075
JinwonKo <115671998+Jinwon-Ko@users.noreply.github.com>
2024-05-20 09:54:47 +0900

    Update get_test_process.py

--- Modeling/SLCD/code/engines/get_test_process.py
+++ Modeling/SLCD/code/engines/get_test_process.py
@@ -4,10 +4,20 @@ import torch
 from torch.utils.data import DataLoader
 from dataloaders.factory import load_test_dataset
 
+from engines.train.train_process import forward_line_detector
 from engines.test.test_process import analysis_line_detection
+from models.networks.line_detector.model import Detector
 
 
 def do_test_process(cfg, model, criterion):
+    if len(os.listdir(cfg.pickle_dir)) == 0:
+        detector = Detector(cfg).cuda()
+        checkpoint = torch.load(cfg.detector_ckpt)
+        detector.load_state_dict(checkpoint['model_state_dict'], strict=False)
+        checkpoint = None  # For memory
+        forward_line_detector(cfg, detector)
+        detector = None  # For memory
+
     checkpoint = torch.load(cfg.init_model)
     model.load_state_dict(checkpoint['model_state_dict'], strict=False)
     checkpoint = None
